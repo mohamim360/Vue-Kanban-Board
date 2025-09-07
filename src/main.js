6ae3aa5706753720946ff3da931da01c8f17646d
mohamim360 <mohamim360@gmail.com>
2025-09-07 21:37:13 +0600

    style

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import "@vueup/vue-quill/dist/vue-quill.snow.css";
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    cssCodeSplit: false, // This can help with CSS loading issues
+  },
+  css: {
+    postcss: './postcss.config.js',
+  }
 })
